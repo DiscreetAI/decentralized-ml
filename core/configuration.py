d3c7d9a0e1e0cd276daf900172e9fa1c24e9cd3d
neeleshdodda44 <ndodda@berkeley.edu>
2018-07-30 23:03:52 -0700

    created/tested/documented Configuration Manager

--- core/configuration.py
+++ core/configuration.py
@@ -1,20 +1,71 @@
+import os
+import json
+import pandas as pd
+import configparser
+import logging
+
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='[ConfigurationManager] %(asctime)s %(levelname)s %(message)s')
+					format='[ConfigurationManager] %(asctime)s %(levelname)s %(message)s')
 
 class ConfigurationManager(object):
 	__instance = None
 
-    @staticmethod
-    def getInstance():
-        """ Static access method. """
-        if ConfigurationManager.__instance == None:
-            ConfigurationManager()
-        return ConfigurationManager.__instance
+	@staticmethod
+	def getInstance():
+		""" Static access method. """
+		if ConfigurationManager.__instance == None:
+			ConfigurationManager()
+		return ConfigurationManager.__instance
 
-    def __init__(self):
-        """ Virtually private constructor. """
-        if ConfigurationManager.__instance != None:
-            raise Exception("This class is a singleton!")
-        else:
-            ConfigurationManager.__instance = self
+	def __init__(self, config_filepath="core/configuration.ini"):
+		""" Virtually private constructor. """
+		if ConfigurationManager.__instance != None:
+			raise Exception("This class is a singleton!")
+		else:
+			ConfigurationManager.__instance = self
+			self.config_filepath = config_filepath
+			self.question_format = "{question} [default = {default}] "
+			self.config = None
+
+	def bootstrap(self, test_input=None):
+		""" Do nothing if config instance already exists. Otherwise, check if core/configuration.ini exists. If not, call 
+			run_setup_mode. Then load the config instance from configuration.ini
+
+			'test_input' is just an easy way to mock out user input for testing """
+		if not self.config:
+			user_input = test_input if test_input else input
+			print(os.listdir('core'))
+			if not os.path.isfile(self.config_filepath):
+				print("found")
+				self.run_setup_mode(user_input)
+			self.config = configparser.ConfigParser()
+			self.config.read(self.config_filepath)
+
+
+	def run_setup_mode(self, user_input):
+		""" Create configuration.ini with the user_input function. In testing, this is a deterministic lambda function. 
+			Otherwise, the user_input function actually draws upon user input. """
+		config = configparser.ConfigParser()
+		rows = [r[1] for r in pd.read_csv("core/questions.csv").iterrows()]
+		for row in rows:
+			question, section, key, default = row['question'], row['section'], row['key'], row['default']
+			answer = user_input(self.question_format.format(question=question, default=default))
+			answer = answer if answer else default
+			if not section in config:
+				config[section] = {}
+			config[section][key] = answer
+		with open(self.config_filepath, 'w') as configfile:
+			config.write(configfile)
+
+	def reset(self):
+		""" Used for cleanup during testing"""
+		os.remove(self.config_filepath)
+
+
+
+
+
+
+
+
---
+++ tests/test_configurationmanager.py
@@ -0,0 +1,53 @@
+import tests.context
+import pytest
+import mock
+from core.configuration import ConfigurationManager
+
+cm = ConfigurationManager()
+
+def setup_default_sanity():
+	""" Ensure that a config file was created using default user input. """
+	cm.bootstrap(test=lambda x: '')
+	assert hasattr(cm, 'config')
+
+def setup_default_worked():
+	""" Actually verify that the config file created using default user input is correct. """
+	config = cm.config
+	assert config.get('GENERAL', 'dataset_path') == 'datasets/mnist'
+	assert config.getint('SCHEDULER', 'num_runners') == 4
+	assert config.getint('SCHEDULER', 'frequency_in_mins') == 1
+	assert config.get('RUNNER', 'weights_directory') == 'weights'
+
+def setup_custom_sanity():
+	""" Ensure that a config file was created using custom user input. """
+	cm.bootstrap(test=lambda x: 'test')
+	assert hasattr(cm, 'config')
+
+def setup_custom_worked():
+	""" Actually verify that the config file created using custom user input is correct. """
+	config = cm.config
+	assert config.get('GENERAL', 'dataset_path') == 'test'
+	assert config.get('SCHEDULER', 'num_runners') == 'test'
+	assert config.get('SCHEDULER', 'frequency_in_mins') == 'test'
+	assert config.get('RUNNER', 'weights_directory') == 'test'
+
+def test_complete_setup_default():
+	""" Verify configuration.ini from default user input is created and correct. """
+	cm.reset()
+	setup_default_sanity()
+	setup_default_worked()
+
+def test_complete_setup_custom():
+	""" Verify configuration.ini from custom user input is created and correct. """
+	cm.reset()
+	setup_custom_sanity()
+	setup_custom_worked()
+
+def test_no_setup_repeat():
+	""" Verify that run_setup_mode is not run again when configuration already exists. Or more simply, that the 
+		configuration has not changed) """
+	cm.reset()
+	cm.bootstrap(test=lambda x: '')
+	setup_default_worked()
+	cm.bootstrap(test=lambda x: 'test')
+	setup_default_worked()
