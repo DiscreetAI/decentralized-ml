4f134a6359ce58b79aaad8cd5891366ecaae9a71
neeleshdodda44 <ndodda@berkeley.edu>
2018-08-06 00:00:01 -0700

    empty config_manager folder not adding -> created during tests

--- tests/test_configurationmanager.py
+++ tests/test_configurationmanager.py
@@ -4,11 +4,6 @@ import mock
 import os
 from core.configuration import ConfigurationManager
 
-def reset(cm):
-	""" Used for cleanup during testing"""
-	if os.path.isfile(cm.config_filepath):
-		os.remove(cm.config_filepath)
-		cm.config = None
 
 def setup_default_sanity(cm):
 	""" Ensure that a config file was created using default user input. """
@@ -36,33 +31,35 @@ def setup_custom_worked(cm):
 
 def test_complete_setup_default():
 	""" Verify configuration.ini from default user input is created and correct. """
+	os.mkdir('tests/artifacts/config_manager')
 	cm = ConfigurationManager.get_instance(config_filepath='tests/artifacts/config_manager/configuration.ini', 
 										   input_function=lambda x: '')
-	
 	setup_default_sanity(cm)
 	setup_default_worked(cm)
-	reset(cm)
+	os.remove(cm.config_filepath)
+	os.rmdir('tests/artifacts/config_manager')
 	assert not os.path.isfile('tests/artifacts/config_manager/configuration.ini')
 
 def test_complete_setup_custom():
 	""" Verify configuration.ini from custom user input is created and correct. """
+	os.mkdir('tests/artifacts/config_manager')
 	cm = ConfigurationManager.get_instance(config_filepath='tests/artifacts/config_manager/configuration.ini', 
 										   input_function=lambda x: 'test')
-
-	print(os.path.isfile('tests/artifacts/config_manager/configuration.ini'))
 	setup_custom_sanity(cm)
-	print(os.path.isfile('tests/artifacts/config_manager/configuration.ini'))
 	setup_custom_worked(cm)
-	reset(cm)
+	os.remove(cm.config_filepath)
+	os.rmdir('tests/artifacts/config_manager')
 	assert not os.path.isfile('tests/artifacts/config_manager/configuration.ini')
 
 def test_no_setup_repeat():
 	""" Verify that run_setup_mode is not run again when configuration already exists. Or more simply, that the 
 		configuration has not changed) """
+	os.mkdir('tests/artifacts/config_manager')
 	cm = ConfigurationManager.get_instance(config_filepath='tests/artifacts/config_manager/configuration.ini', 
 										   input_function=lambda x: '')
 	setup_default_worked(cm)
 	cm = ConfigurationManager.get_instance(config_filepath='tests/artifacts/config_manager/configuration.ini', 
 										   input_function=lambda x: 'test')
 	setup_default_worked(cm)
-	reset(cm)
+	os.remove(cm.config_filepath)
+	os.rmdir('tests/artifacts/config_manager')
