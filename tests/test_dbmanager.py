4e0f55d70b4c60055bfdb4d53650f9dcf149f2d3
neeleshdodda44 <ndodda@berkeley.edu>
2018-08-11 15:54:35 -0700

    fixed tests

--- tests/test_dbmanager.py
+++ tests/test_dbmanager.py
@@ -16,16 +16,10 @@ def test_empty_category_labels(db_manager):
 
 def test_add_works(db_manager):
 	db_manager._reset()
-	row = {
-			'data_provider': ['Facebook Profile Data'],
-			'category': ['social_media']
-		} 
-	expected = pd.DataFrame(data=row)
 	db_manager.add_labels(['Facebook Profile Data'], ['social_media'])
 	actual = db_manager.get_labels()
-	print(actual)
-	print(expected)
-	assert expected.equals(actual)
+	assert list(actual['data_provider']) == ['Facebook Profile Data']
+	assert list(actual['category']) == ['social_media']
 
 def test_get_data_providers_with_category(db_manager):
 	db_manager._reset()
@@ -36,8 +30,7 @@ def test_get_data_providers_with_category(db_manager):
 	expected = pd.DataFrame(data=row)
 	db_manager.add_labels(['Facebook Profile Data', 'Fitbit Calories Burned'], ['social_media', 'fitness'])
 	actual = db_manager.get_data_providers_with_category('social_media')
-	print(actual)
-	print(expected)
-	assert expected.equals(actual)
+	assert list(actual['data_provider']) == ['Facebook Profile Data']
+	assert list(actual['category']) == ['social_media']
 
 
