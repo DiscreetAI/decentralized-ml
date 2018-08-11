96e4594f490bf78e9af9409e95630008551b7742
neeleshdodda44 <ndodda@berkeley.edu>
2018-08-11 14:44:05 -0700

    finished DB Manager with RDS

--- core/DBManager.py
+++ core/DBManager.py
@@ -5,47 +5,29 @@ import pandas as pd
 
 
 class DBManager(object):
-	__instance = None
-
-	@staticmethod
-	def get_instance():
-		return __instance
 
 	def __init__(self, config_filepath = 'database_config.json'):
-		if DBManager.__instance:
-			raise Exception("This class is a singleton!")
-		else:
-			DBManager.__instance = self
 		app = Flask(__name__)
 		with open(config_filepath) as f:
 			POSTGRES = json.load(f)
 		app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 		self.db = SQLAlchemy(app)
+		self.table_name = POSTGRES['table_name']
+		self._reset()
 
 	def get_labels(self):
-		try:
-			labels = pd.read_sql_query("select * from category_labels", self.db.engine)
-			return labels
-		except:
-			raise Exception('Table "category_labels" does not exist!')
+		return pd.read_sql_query("select * from {table_name}".format(table_name=self.table_name), self.db.engine)
 
-	def add_label(self, data_provider, category):
-		row = {
-			'data_provider': [data_provider],
-			'category': [category]
-		}
-		labels = pd.DataFrame(data=row)
-		labels.to_sql(name='category_labels', con=self.db.engine, if_exists='append', index=False)
+	def add_labels(self, data_providers, categories):
+		rows = {'data_provider': data_providers, 'category': categories} 
+		label = pd.DataFrame(data=rows)
+		label.to_sql(name=self.table_name, con=self.db.engine, if_exists='append', index=False)
 
 	def get_data_providers_with_category(self, category):
-		try:
-			query = "select * from category_labels where category = '{category}'".format(category=category)
-			return list(pd.read_sql_query(query, self.db.engine)['data_provider'])
-		except:
-			raise Exception('Table "category_labels" does not exist!')
+		return pd.read_sql_query("select * from {table_name} where category = '{category}'"
+			.format(category=category, table_name=self.table_name), self.db.engine)
 
 	def _reset(self):
-
-		labels = pd.DataFrame(columns=['data_provider', 'category'])
-		labels.to_sql(name='category_labels', con=self.db.engine, if_exists='replace', index=False)
+		label = pd.DataFrame(columns=['data_provider', 'category'])
+		label.to_sql(name=self.table_name, con=self.db.engine, if_exists='replace', index=False)
 		
