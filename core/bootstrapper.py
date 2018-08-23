95b70fc0f08b50d7b7efa1607270b5b7f1266a03
neeleshdodda44 <ndodda@berkeley.edu>
2018-08-22 18:01:57 -0700

    Add skeleton for bootstrapper

---
+++ core/bootstrapper.py
@@ -0,0 +1,11 @@
+def bootstrap():
+    """
+    Bootstraps the data provider unix service.
+
+    It instantiates the Configuration Manager, Dataset Manager, and the Execution
+    Pipeline. It also checks the Communication Module is up and running.
+
+    Note: for now, both the Execution Pipeline's Listener and Communication Module
+    are debug versions based on flask.
+    """
+    pass
