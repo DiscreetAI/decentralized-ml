75d74375031d3f99e022445dbf7ca5c8b0a120e9
neeleshdodda44 <ndodda@berkeley.edu>
2018-09-07 16:17:18 -0700

    added more documentation

--- core/dataset_classifier.py
+++ core/dataset_classifier.py
@@ -4,16 +4,19 @@ from core.db_client import DBClient
 
 
 class DatasetClassifier(object):
-	def __init__(self, label=None):
-		self.label = label
+	
+	def __init__(self):
+		"""
+		Initalizes instance of Dataset Classifier. Only 
+		"""
 		self.db_client = DBClient()
 
-	def _classify_dataset(self):
-		if not self.label:
+	def label_dataset(self, name=None, label=None):
+		"""
+		Labels dataset in DB. Only supports hardcoded names and labels at this time.
+		"""
+		if name and label:
+			self.db_client.add_labels([name], [label])
+		else:
 			raise Exception('Classifier is not working at this time.')
-		return self.label
-
-	def label_dataset(self, name):
-		label = self._classify_dataset()
-		self.db_client.add_labels([name], [label])
 
