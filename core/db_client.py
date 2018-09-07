9e63da638d7ccf20cbcd0ea50334c42c30beb605
neeleshdodda44 <ndodda@berkeley.edu>
2018-09-07 12:31:23 -0700

    added db client

---
+++ core/db_client.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import json
+import pandas as pd
+
+
+class DBClient(object):
+
+	def __init__(self, config_filepath = 'database_config.json'):
+		"""
+		Set up DBClient with corresponding database credentials
+		"""
+		app = Flask(__name__)
+		with open(config_filepath) as f:
+			POSTGRES = json.load(f)
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+		app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+		self.db = SQLAlchemy(app)
+		self.table_name = POSTGRES['table_name']
+		self._reset()
+
+	def get_labels(self):
+		"""
+		Get category_labels table
+		"""
+		return pd.read_sql_query("select * from {table_name}".format(table_name=self.table_name), self.db.engine)
+
+	def add_labels(self, data_providers, categories):
+		"""
+		Append category labels to the category_labels table, where categories[i] corresponds to the labeled category
+		for data_providers[i]
+		"""
+		rows = {'data_provider': data_providers, 'category': categories} 
+		label = pd.DataFrame(data=rows)
+		label.to_sql(name=self.table_name, con=self.db.engine, if_exists='append', index=False)
+
+	def get_data_providers_with_category(self, category):
+		"""
+		Get a list of data providers with the given category.
+		"""
+		return pd.read_sql_query("select * from {table_name} where category = '{category}'"
+			.format(category=category, table_name=self.table_name), self.db.engine)
+
+	def _reset(self):
+		label = pd.DataFrame(columns=['data_provider', 'category'])
+		label.to_sql(name=self.table_name, con=self.db.engine, if_exists='replace', index=False)
+		
