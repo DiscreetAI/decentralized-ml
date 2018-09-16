ac1bc94f7c2ce3d3591c95807427c00113b9749a
neeleshdodda44 <ndodda@berkeley.edu>
2018-09-16 16:25:34 -0700

    back to integer labeler

--- data/iterators.py
+++ data/iterators.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import pandas as pd
 import keras
+from sklearn.model_selection import train_test_split
+import warnings
 
 
 def count_datapoints(dataset_path):
@@ -49,6 +51,38 @@ def create_test_dataset_iterator(dataset_path, count, split=0.2, batch_size=1, \
         yield from _create_dataset_iterator(dataset_path, count*split, "test", \
             batch_size, labeler)
 
+def reverse_readline(filename, buf_size=8192):
+    """a generator that returns the lines of a file in reverse order"""
+    with open(filename) as fh:
+        segment = None
+        offset = 0
+        fh.seek(0, os.SEEK_END)
+        file_size = remaining_size = fh.tell()
+        while remaining_size > 0:
+            offset = min(file_size, offset + buf_size)
+            fh.seek(file_size - offset)
+            buffer = fh.read(min(remaining_size, buf_size))
+            remaining_size -= buf_size
+            lines = buffer.split('\n')
+            # the first line of the buffer is probably not a complete line so
+            # we'll save it and append it to the last line of the next buffer
+            # we read
+            if segment is not None:
+                # if the previous chunk starts right from the beginning of line
+                # do not concact the segment to the last line of new chunk
+                # instead, yield the segment first 
+                if buffer[-1] is not '\n':
+                    lines[-1] += segment
+                else:
+                    yield segment
+            segment = lines[0]
+            for index in range(len(lines) - 1, 0, -1):
+                if len(lines[index]):
+                    yield lines[index]
+        # Don't yield None if the file was empty
+        if segment is not None:
+            yield segment
+
 def _create_dataset_iterator(dataset_path, max_count, iter_type, batch_size, labeler):
     """
     Returns an iterator of batches of size B containing all features of the data.
@@ -58,20 +92,32 @@ def _create_dataset_iterator(dataset_path, max_count, iter_type, batch_size, lab
     NOTE: labeler is now a string that refers to a column name
     """
     assert iter_type in ['train', 'test'], "'iter_type' parameter is invalid."
-    assert isinstance(labeler, str), "The labeler must be a string!"
     if iter_type == 'train':
         directories = os.listdir(dataset_path)
     elif iter_type == 'test':
         directories = reversed(os.listdir(dataset_path))
 
-    num_batches = int(max_count/batch_size)
-    total_points = int(max_count/batch_size) * batch_size
+    count = 0
     for filename in directories:
         if not filename.endswith(".csv"): continue
         full_path = os.path.join(dataset_path, filename)
-        file = pd.read_csv(full_path).head(total_points)
-        assert labeler in file.columns, "Invalid column name for dataset"
-        X, y = (file.drop(labeler, axis=1), pd.DataFrame(file[labeler]))
-        X_split, y_split = np.split(X, num_batches), np.split(y, num_batches)
-        for batch in zip(X_split,y_split):
-            yield(batch)
+        f = reverse_readline(full_path)
+        batch = []
+        for line in f:
+            if count >= max_count:
+                return
+            if len(batch) == batch_size:
+                if batch_size == 1:
+                    tup = batch[0]
+                    yield (np.expand_dims(tup[0], axis=0), \
+                        np.expand_dims(tup[1], axis=0))
+                else:
+                    X_list, y_list = list(), list()
+                    for x, _ in batch: X_list.append(x)
+                    for _, y in batch: y_list.append(y)
+                    yield (np.array(X_list), np.array(y_list))
+                batch = []
+            line = line.split(",")
+            line = line[0:labeler] + line[labeler + 1:], line[labeler]
+            batch.append(line)
+            count += 1
