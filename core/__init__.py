bcf5e9e81bddc1f426b5621d63b87440f1ae78a8
neeleshdodda44 <ndodda@berkeley.edu>
2018-10-07 15:54:45 -0700

    after review commit

--- core/__init__.py
+++ core/__init__.py
@@ -1,48 +1,55 @@
-import asyncio
-from ipywidgets import *
+from ipywidgets import widgets
 from IPython.display import display
-from core.category_component import *
+from core.category_component import CategoryComponent
 from core.db_client import DBClient
 
-def get_category():
-    """
-    Displays:
-        Prompt to get category from user
-        ED directories to user or failure with the reason
-        Some decoration has been added to this method
-    """
-    def orchestrate(sender):
-        category_text = category_widget.value.strip().lower()
-        db_client = DBClient()
-        category_component = CategoryComponent(db_client)
-        cc_dict = category_component.get_ed_with_category(category_text)
-        # more coming here :D
+"""
+	Global variables
+"""
+db_client = DBClient()
+category_component = CategoryComponent(db_client)
 
-    decoration()
-    category_text = None
-    category_widget = widgets.Text(
-        value=None,
-        placeholder='',
-        description='Category:',
-        disabled=False,
 
-    )
-    button = widgets.Button(description='Submit')
-    button.on_click(orchestrate)
-    display(category_widget)
-    display(button)
+def get_ed_directories():
+	"""
+	Displays:
+		Prompt to get category from user.
+		ED directories to user or failure.
+	"""
+	def orchestrate(sender):
+		"""
+		This method is triggered by the button.on_click event
+    	It uses the current value of category_widget variable
+    	to get the dictionary with ed.
+		"""
+		category_text = category_widget.value.strip().lower()
+		cc_dict = category_component.get_ed_with_category(category_text)
+		# TODO: finish what to do with the cc_dict
+
+	category_widget = widgets.Text(
+		value=None,
+		placeholder='',
+		description='Category:',
+		disabled=False,
+
+	)
+	button = widgets.Button(description='Submit')
+	button.on_click(orchestrate)
+	display(category_widget)
+	display(button)
 
 def decoration():
-    """
-    Displays:
-    	A decoration for the notebook, logo of Dagora
-    """
-    file = open("core/images/DA_nowords.png", "rb")
-    image = file.read()
-    image_widget = widgets.Image(
-        value=image,
-        format='png',
-        width=300,
-        height=400,
-    )
-    display(image_widget)
+	"""
+	Displays:
+		A decoration for the notebook, logo of Dagora.
+	"""
+	file = open("core/images/DA_nowords.png", "rb")
+	image = file.read()
+	image_widget = widgets.Image(
+		value=image,
+		format='png',
+		width=300,
+		height=400,
+	)
+	display(image_widget)
+
