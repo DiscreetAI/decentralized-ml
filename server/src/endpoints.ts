d95aca359cbc7a85109f2a0552a29b8a7b78e431
neeleshdodda44 <ndodda@berkeley.edu>
2019-03-31 22:05:36 -0700

    Add pseudocode

--- server/src/endpoints.ts
+++ server/src/endpoints.ts
@@ -9,17 +9,29 @@ app.get('/new', function(req, res) {
   console.log('Starting new DML Session...');
   res.end("Starting new DML Session...");
 
+  // 1. If server is BUSY, error. Otherwise, mark the service as BUSY.
 
+  // 2. Set the internal round variable to 1, reset the number of nodes
+  //    averaged to 0.
 
-  // 1. Mark the service as BUSY.
-
-  // 2. Decode new message from Explora with:
+  // 3. Decode new message from Explora with:
   //    - Metadata (user, repo, logging info)
-  //    - Model + weights + optimizer (h5 binary format)
-  //    - Hyperparameters
-  //    - Selection Criteria
+  //    - Model + weights + optimizer (in .h5 binary format)
+  //    - Hyperparameters (batch size, learning rate, decay)
+  //    - Selection Criteria (i.e., nodes with more than 1000 data points)
+  //    - Continuation Criteria (i.e., 80% of selected nodes weights averaged)
+  //    - Termination Criteria (i.e., 20 FL rounds completed)
 
-  // 3.
+  // 4. Decode the weights and store them in memory.
+  //    (Will be used for running weighted average.)
+
+  // 5. According to the 'Selection Criteria', choose clients to forward
+  //    training messages to and kickstart a DML Session with round # 1
 });
+
+// TODO: Put a loop somewhere to support timing out. For example, if the service
+// hasn't received a message for more than 15 minutes, it resets all connections,
+// and resumes training from the last weights it has access to. It should retry
+// this 3 or so times.
 
 export { app };
--- server/src/server.ts
+++ server/src/server.ts
@@ -14,6 +14,40 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log(`Received: ${message}`);
     ws.send(JSON.stringify({ answer: 42 }));
+
+    // One of the clients has sent us a message.
+    // Right now we can only receive "new_weights" types of messages.
+
+    // 1. If the round in the received message doesn't match the internal round,
+    //    then discard the message.
+
+    // 2 Lock section/variables that will be changed...
+
+    // 2. If 'Continuation Criteria' isn't met...
+
+        // 2.a. Do running weighted average on the new weights.
+
+        // 2.b. Update the number of nodes averaged (+1)
+
+    // 3. If 'Continuation Criteria' is met...
+
+        // 3.a. Update the number of nodes averaged (+1)
+
+        // 3.b. Log the resulting weights for the user (for this round)
+
+        // 3.c. Update round number (+1)
+
+        // 3.d. If 'Termination Criteria' isn't met, then kickstart a new FL round
+        //      NOTE: We need a way to swap the weights from the initial message
+        //      in node............
+
+    // 4. If 'Termination Criteria' is met...
+
+        // 4.a. Log the resulting weights for the user (for this session)
+
+        // 4.b. Reset all state in the service and mark BUSY as false
+
+    // 5. Release section/variables that were changed...
   });
 
   console.log("New connection!");
@@ -23,3 +57,5 @@ wss.on('connection', function connection(ws) {
 server.listen(process.env.PORT, function() {
   console.log(`http/ws server listening on ${process.env.PORT}`);
 });
+
+// NOTE: We need to implement some ping-pong/ways to deal with disconnections.
