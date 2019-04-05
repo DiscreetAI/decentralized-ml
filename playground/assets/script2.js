fa54aa653453bee5574cbddfe565577726ac4615
neeleshdodda44 <ndodda@berkeley.edu>
2019-04-05 16:25:38 -0700

    Update playground and new model

--- playground/assets/script2.js
+++ playground/assets/script2.js
@@ -8,7 +8,7 @@ async function getData() {
 }
 
 async function getModel() {
-  const MODEL_URL = 'http://localhost:5000/playground/server/model.json';
+  const MODEL_URL = 'http://localhost:5000/server/model.json';
   const model = await tf.loadLayersModel(MODEL_URL);
   console.log("Model loaded!", model);
   return model;
@@ -65,7 +65,7 @@ async function retrainModel(model, data) {
   const metrics = ['loss', 'acc'];
 
   const BATCH_SIZE = 128;
-  const TRAIN_DATA_SIZE = 5000;
+  const TRAIN_DATA_SIZE = 8000;
   //const TEST_DATA_SIZE = 1000;
 
   const [trainXs, trainYs] = tf.tidy(() => {
@@ -108,66 +108,66 @@ async function _evaluateModel(model, data) {
   return [labels, preds];
 }
 
-function makeModel() {
-  const model = tf.sequential();
-
-  const IMAGE_WIDTH = 28;
-  const IMAGE_HEIGHT = 28;
-  const IMAGE_CHANNELS = 1;
-
-  // In the first layer of out convolutional neural network we have
-  // to specify the input shape. Then we specify some paramaters for
-  // the convolution operation that takes place in this layer.
-  model.add(tf.layers.conv2d({
-    inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
-    kernelSize: 5,
-    filters: 8,
-    strides: 1,
-    activation: 'relu',
-    kernelInitializer: 'varianceScaling'
-  }));
-
-  // The MaxPooling layer acts as a sort of downsampling using max values
-  // in a region instead of averaging.
-  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-
-  // Repeat another conv2d + maxPooling stack.
-  // Note that we have more filters in the convolution.
-  model.add(tf.layers.conv2d({
-    kernelSize: 5,
-    filters: 16,
-    strides: 1,
-    activation: 'relu',
-    kernelInitializer: 'varianceScaling'
-  }));
-  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-
-  // Now we flatten the output from the 2D filters into a 1D vector to prepare
-  // it for input into our last layer. This is common practice when feeding
-  // higher dimensional data to a final classification output layer.
-  model.add(tf.layers.flatten());
-
-  // Our last layer is a dense layer which has 10 output units, one for each
-  // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
-  const NUM_OUTPUT_CLASSES = 10;
-  model.add(tf.layers.dense({
-    units: NUM_OUTPUT_CLASSES,
-    kernelInitializer: 'varianceScaling',
-    activation: 'softmax'
-  }));
-
-
-  // Choose an optimizer, loss function and accuracy metric,
-  // then compile and return the model
-  const optimizer = tf.train.adam();
-  model.compile({
-    optimizer: optimizer,
-    loss: 'categoricalCrossentropy',
-    metrics: ['accuracy'],
-  });
-
-  return model;
-}
+// function makeModel() {
+//   const model = tf.sequential();
+//
+//   const IMAGE_WIDTH = 28;
+//   const IMAGE_HEIGHT = 28;
+//   const IMAGE_CHANNELS = 1;
+//
+//   // In the first layer of out convolutional neural network we have
+//   // to specify the input shape. Then we specify some paramaters for
+//   // the convolution operation that takes place in this layer.
+//   model.add(tf.layers.conv2d({
+//     inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+//     kernelSize: 5,
+//     filters: 8,
+//     strides: 1,
+//     activation: 'relu',
+//     kernelInitializer: 'varianceScaling'
+//   }));
+//
+//   // The MaxPooling layer acts as a sort of downsampling using max values
+//   // in a region instead of averaging.
+//   model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+//
+//   // Repeat another conv2d + maxPooling stack.
+//   // Note that we have more filters in the convolution.
+//   model.add(tf.layers.conv2d({
+//     kernelSize: 5,
+//     filters: 16,
+//     strides: 1,
+//     activation: 'relu',
+//     kernelInitializer: 'varianceScaling'
+//   }));
+//   model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+//
+//   // Now we flatten the output from the 2D filters into a 1D vector to prepare
+//   // it for input into our last layer. This is common practice when feeding
+//   // higher dimensional data to a final classification output layer.
+//   model.add(tf.layers.flatten());
+//
+//   // Our last layer is a dense layer which has 10 output units, one for each
+//   // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
+//   const NUM_OUTPUT_CLASSES = 10;
+//   model.add(tf.layers.dense({
+//     units: NUM_OUTPUT_CLASSES,
+//     kernelInitializer: 'varianceScaling',
+//     activation: 'softmax'
+//   }));
+//
+//
+//   // Choose an optimizer, loss function and accuracy metric,
+//   // then compile and return the model
+//   const optimizer = tf.train.adam();
+//   model.compile({
+//     optimizer: optimizer,
+//     loss: 'categoricalCrossentropy',
+//     metrics: ['accuracy'],
+//   });
+//
+//   return model;
+// }
 
 function _lowerCaseToCamelCase(str) {
   return str.replace(/_([a-z])/g, function (g) { return g[1].toUpperCase(); });
