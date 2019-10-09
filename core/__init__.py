724454ed7e7ab0a22080263cfb54389db711fc0d
neeleshdodda44 <ndodda@berkeley.edu>
2019-10-09 16:55:06 -0700

    Cloud integration (#21)
    * Cloud update

--- core/__init__.py
+++ core/__init__.py
@@ -1,40 +1,66 @@
-from core.orchestrator import Orchestrator
-from ipywidgets import widgets
-from IPython.display import display, Image
-from core.category_component import CategoryComponent
-from core.db_client import DBClient
-import pandas as pd
-from core.ed_component import EDComponent
-from core.blockchain_client import BlockchainClient
-from core.dml_client import DMLClient
-from core.status_server_client import StatusServerClient
-import numpy as np
-import configparser
-import logging
+import sys
 
+from twisted.python import log
+from twisted.internet import reactor
+import json
+import base64
 
-class Explora(Orchestrator):
-    """
-    Directly called by user to set up Orchestrator and components it uses.
-    """
-    def __init__(self, config_filepath='core/configuration.ini'):
-        """
-        Wrapper around Orchestrator so that:
+from autobahn.twisted.websocket import WebSocketClientProtocol
+from autobahn.twisted.websocket import WebSocketClientFactory
 
-        1. User simply has to call "explora.<command>" to execute functions
-           in Orchestrator
-        2. We maintain use of dependency injection, which simplifies testing.
-        """
-        logger = logging.getLogger()
-        logger.setLevel(logging.CRITICAL)
-        config = configparser.ConfigParser()
-        config.read(config_filepath)
-        db_client = DBClient(config)
-        blockchain_client = BlockchainClient(config)
-        dml_client = DMLClient(config)
-        status_server_client = StatusServerClient(config)
-        category_component = CategoryComponent(db_client, blockchain_client)
-        ed_component = EDComponent()
-        Orchestrator.__init__(self, category_component, ed_component, \
-            dml_client, status_server_client)
+class Explora(object):
 
+    def __init__(self):
+        #log.startLogging(sys.stdout)
+        self.CLOUD_BASE_URL = ".au4c4pd2ch.us-west-1.elasticbeanstalk.com"
+        self.CLOUD_NODE_PORT = 80
+        self.factory = WebSocketClientFactory()
+
+    def start_new_session(self, repo_id, model, hyperparams, percentage_averaged, max_rounds):
+        self.CLOUD_NODE_HOST = repo_id + self.CLOUD_BASE_URL
+
+        model.save("core/model/my_model.h5")
+        with open("core/model/my_model.h5", mode='rb') as file:
+            file_content = file.read()
+            encoded_content = base64.b64encode(file_content)
+            h5_model = encoded_content.decode('ascii')
+
+        NEW_MESSAGE = {
+            "type": "NEW_SESSION",
+            "repo_id": repo_id,
+            "h5_model": h5_model,
+            "hyperparams": hyperparams,
+            "selection_criteria": {
+                "type": "ALL_NODES",
+            },
+            "continuation_criteria": {
+                "type": "PERCENTAGE_AVERAGED",
+                "value": percentage_averaged
+            },
+            "termination_criteria": {
+                "type": "MAX_ROUND",
+                "value": max_rounds
+            }
+        }
+
+        NEW_CONNECTION_MESSAGE = {
+            "type": "REGISTER",
+            "node_type": "dashboard",
+        }
+
+        class NewSessionTestProtocol(WebSocketClientProtocol):
+            def onOpen(self):
+                json_data = json.dumps(NEW_CONNECTION_MESSAGE)
+                self.sendMessage(json_data.encode())
+                json_data = json.dumps(NEW_MESSAGE)
+                self.sendMessage(json_data.encode())
+
+            def onMessage(self, payload, isBinary):
+                if isBinary:
+                    print("Binary message received: {0} bytes".format(len(payload)))
+                else:
+                    print("Text message received: {0}".format(payload.decode('utf8')))
+
+        self.factory.protocol = NewSessionTestProtocol
+        reactor.connectTCP(self.CLOUD_NODE_HOST, self.CLOUD_NODE_PORT, self.factory)
+        reactor.run()
