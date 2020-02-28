812ba14e3c415269d04571bf2dce89f583f845eb
Neelesh Dodda <ndodda@berkeley.edu>
2020-02-27 18:48:58 -0800

    Update Cloud Node Testing Utils (#65)

--- cloud-node/tools/start_new_session.py
+++ cloud-node/tools/start_new_session.py
@@ -13,11 +13,12 @@ else:
     explora = Explora()
     repo_id = "99885f00eefcd4107572eb62a5cb429a"
 
+    model_path = "../tests/artifacts/{}"
     if library == 'ios':
-        h5_model_path = "assets/ios_model.h5"
+        h5_model_path = model_path.format("ios_model.h5")
         loop.run_until_complete(explora.start_new_session(repo_id, "IOS", h5_model_path))
     elif library == 'python':
-        h5_model_path = "assets/my_model.h5"
+        h5_model_path = model_path.format("my_model.h5")
         loop.run_until_complete(explora.start_new_session(repo_id, "PYTHON", h5_model_path))
     else:
         print("Library not supported!")
