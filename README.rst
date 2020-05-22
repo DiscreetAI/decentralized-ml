4e2f509e89624717403213c4eaf0cfd72ef4afdf
Ashwinee Panda <kiddyboots216@berkeley.edu>
2020-05-22 03:02:19 -0700

    Update README (#83)

--- README.rst
+++ README.rst
@@ -1,11 +1,59 @@
 Decentralized Machine Learning
 ==============================
-
+.. image:: https://discreetai.com/wp-content/uploads/2019/12/dataagora-logo-blck-1-1.png
 |build| |docs|
 
-Full stack service enabling decentralized machine learning on private data.
+**DiscreetAI is a full stack service enabling machine learning on private data.**
 
+Documentation
+----
 Check out the documentation `here <https://discreetai.readthedocs.io/en/latest/>`_!
+
+Documentation for the DiscreetAI iOS library is coming soon!
+
+Starting a Training Session
+----
+1. You can try DiscreetAI out by making an account `here <https://beta.discreetai.com/signup>`_.
+
+.. image:: images/signup.png
+
+
+
+2. You can immediately try out the demo repo, which is set up for you right after registration!
+
+.. image:: images/demo-repo.png
+
+
+
+3. Then you can go ahead train on our iOS test devices in Explora, a customized Jupyter Notebook that starts your training session.
+
+.. image:: images/demo-explora.png
+
+
+
+4. Or, you can create your own repo.
+
+.. image:: images/new-repo.png
+
+
+
+5. With the ability to set up your own clients with our iOS library, Python datacenter or Javascript library!
+
+.. image:: images/test-repo.png
+
+
+
+6. And you can train with your own clients using your own models and settings.
+
+.. image:: images/test-explora.png
+
+Clients
+----
+- To get started developing iOS apps that can generate insights from user data in a secure way, follow these `instructions <https://github.com/DiscreetAI/ios-library/blob/master/README.md>`_.
+
+- If you want to play around with testing models by simply running a Google Colab notebook instead of deploying to a device, see `here <https://github.com/DiscreetAI/decentralized-ml/blob/master/datacenter/README.md>`_.
+
+- To start developing models in Javascript, install our NPM library `here <https://github.com/DiscreetAI/decentralized-ml/blob/master/dml-library-js/README.md>`_.
 
 .. |docs| image:: https://readthedocs.org/projects/docs/badge/?version=latest
     :alt: Documentation Status
--- dashboard-ui/src/components/repo/repoMetadata.js
+++ dashboard-ui/src/components/repo/repoMetadata.js
@@ -9,6 +9,7 @@ class RepoMetadata extends Reflux.Component {
 
         this.repoData = this.props.repoData;
         this.copyRepoIDToClipboard = this.copyRepoIDToClipboard.bind(this);
+        this.copyApiKeyToClipboard = this.copyApiKeyToClipboard.bind(this);
     }
 
     copyRepoIDToClipboard() {
@@ -22,8 +23,19 @@ class RepoMetadata extends Reflux.Component {
         document.body.removeChild(dummy);
     }
 
+    copyApiKeyToClipboard() {
+        // const { match: { params } } = this.props;
+        // const repoId = params.repoId;
+        var dummy = document.createElement("textarea");
+        document.body.appendChild(dummy);
+        dummy.value = this.repoData.ApiKey;
+        dummy.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummy);
+    }
+
     render() {
-        if (!this.repoData.isDemo) {
+        if (!this.repoData.IsDemo) {
             return (
                 <div className="row">
                     <div className="col-1"></div>
@@ -32,14 +44,14 @@ class RepoMetadata extends Reflux.Component {
                     <p>{this.repoData.Description}</p>
                     </div>
                     <div className="col-5 padding">
-                        <h6> <b>REPO ID:  <span className="text-success">{ this.repoData.Id }</span></b>   <button class="btn btn-xs btn-dark copy" onClick={this.copyRepoIDToClipboard}>Copy to Clipboard</button></h6>  
+                        <h6> <b>REPO ID:  <span className="text-success">{ this.repoData.Id }</span></b>   <button class="btn btn-xs btn-dark copy" onClick={this.copyRepoIDToClipboard}>Copy to Clipboard</button></h6> 
+                        <h6> <b>API KEY:  <span className="text-success">{ this.repoData.ApiKey }</span></b>   <button class="btn btn-xs btn-dark copy" onClick={this.copyApiKeyToClipboard}>Copy to Clipboard</button></h6>  
                     </div>
                     <div className="col-2 text-right">
                     <RepoStatus repoId={this.repoData.Id} isDashboard={false}/>
                     </div>
               </div>
             )
-            return 
         } else {
             return (
                 <div className="row">
